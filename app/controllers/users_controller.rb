3f90e78c77694a41239a169a8a795309640cd49f
osk <oosako.7@gmail.com>
2019-12-25 13:29:52 +0000

    user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :show]
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
   end
@@ -17,7 +18,7 @@ class UsersController < ApplicationController
       flash[:success]="ユーザーを登録しました。"
       redirect_to @user
     else
-      flash[:danger]='ユーザの登録に失敗しました。'
+      flash.now[:danger]='ユーザの登録に失敗しました。'
       render :new
     end
 
