d010da144c4ad8c2000714e53244414a56dcf3fd
osk <oosako.7@gmail.com>
2019-12-28 21:17:02 +0000

    follow,unfollow

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,12 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
 
   get 'signup', to: 'users#new'
-  resources :users, only: [:index, :create, :show]
+  resources :users, only: [:index, :create, :show,:new] do
+    member do
+      get :followings
+      get :followers
+    end
+  end
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 end
